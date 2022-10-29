535bb3fed7d4fca8101ac453c78a97dc2f461176
jeremycfwong <71006547+jeremycfwong@users.noreply.github.com>
2022-10-30 01:39:30 +0800

    Add link between display and button and implement clear button
    as title

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ function operate(oper, v1, v2){
     return oper(v1, v2);
 }
 
+var calValues = document.querySelector("#values")
+
+calValues.addEventListener('click', event =>{
+    if (event.target.tagName === 'BUTTON'){
+        if (event.target.id == 'clear'){
+            clearDisplay()
+        } else if (event.target.id != "="){
+            updateDisplay(event.target.id)
+        }  
+    }
+})
+
+
+function updateDisplay(unit){
+    var number = document.getElementById("display").innerText
+    
+    if (number != '-------'){
+        unit = number + unit
+    }
+
+    
+    document.getElementById("display").innerText = unit
+}
+
+function clearDisplay() {
+    document.getElementById("display").innerText = '-------'
+}
