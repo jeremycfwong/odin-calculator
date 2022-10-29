dc49d23ceed229d5d0ac15c0e3bd55f0910263aa
jeremycfwong <71006547+jeremycfwong@users.noreply.github.com>
2022-10-30 01:20:22 +0800

    Add basic calculator set up
    Add the basic html and js logic of calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(v1, v2){
+    return v1 + v2;
+}
+
+function subtract(v1, v2){
+    return v1 - v2;
+}
+
+function multiply(v1, v2){
+    return v1 * v2;
+}
+
+function divide(v1, v2){
+    return v1 / v2;
+}
+
+function operate(oper, v1, v2){
+    return oper(v1, v2);
+}
+
