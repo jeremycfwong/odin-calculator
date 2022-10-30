7c57a4e18bad136cd766d0d0a5a7887ef5d5ed8b
jeremycfwong <71006547+jeremycfwong@users.noreply.github.com>
2022-10-30 11:49:58 +0800

    Implement logic to allow operator chaining
    Implemented logic to allow the calculator to be functional when user
    is chaining equations

--- script.js
+++ script.js
@@ -14,19 +14,64 @@ function divide(v1, v2){
     return v1 / v2;
 }
 
-function operate(oper, v1, v2){
-    return oper(v1, v2);
+function operate(operator, v1, v2){
+    switch (operator){
+        case '+':
+            return add(v1,v2);
+        case '-':
+            return subtract(v1,v2);
+        case '*':
+            return multiply(v1,v2);
+        case '/':
+            return divide(v1,v2);
+    }
 }
 
 var calValues = document.querySelector("#values")
+var value1 = NaN
+var operator = ''
+var chain = false
 
 calValues.addEventListener('click', event =>{
     if (event.target.tagName === 'BUTTON'){
-        if (event.target.id == 'clear'){
-            clearDisplay()
-        } else if (event.target.id != "="){
-            updateDisplay(event.target.id)
-        }  
+        switch (event.target.id){
+            case 'clear':
+                clearDisplay();
+                value1 = NaN
+
+                break;
+            case '=':
+                if(operator != '' && !isNaN(value1)){
+                    value1 = operate(operator, value1,fetchValue())
+                    document.getElementById("display").innerText = value1
+                    operator = ''
+                }
+                break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                if(isNaN(value1)){
+                    operator = event.target.id
+                    value1 = fetchValue() 
+                } else if (operator != '' && !isNaN(value1)){
+                    value1 = operate(operator, value1,fetchValue())
+                    document.getElementById("display").innerText = value1
+                    operator = event.target.id
+                    chain = true
+                    break;
+                } else {
+                    operator = event.target.id
+                }
+                break;
+            default:
+                if (chain){
+                    clearDisplay()
+                    chain = false
+                }
+                updateDisplay(event.target.id)
+                break;
+        }
     }
 })
 
@@ -38,10 +83,15 @@ function updateDisplay(unit){
         unit = number + unit
     }
 
-    
     document.getElementById("display").innerText = unit
 }
 
 function clearDisplay() {
     document.getElementById("display").innerText = '-------'
+}
+
+function fetchValue(){
+    var result = Number(document.getElementById("display").innerText)
+    clearDisplay()
+    return result
 }
